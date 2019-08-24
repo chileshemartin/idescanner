d922a01d24aa4335e7ec5c010df99d85fb7cc206
Martin <chileshemartin@gmail.com>
2019-08-24 11:39:30 +0200

    fix: fix the crushing issue when launching the app

--- app/src/main/java/com/example/idescanner/MainActivity.java
+++ app/src/main/java/com/example/idescanner/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.idescanner;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,30 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
         //Set Event
         setSingleEvent(mainGrid);
-        //setToggleEvent(mainGrid);
+        setToggleEvent(mainGrid);
     }
 
     private void setToggleEvent(GridLayout mainGrid) {
-//        //Loop all child item of Main Grid
-//        for (int i = 0; i < mainGrid.getChildCount(); i++) {
-//            //You can see , all child item is CardView , so we just cast object to CardView
-//            final CardView cardView = (CardView) mainGrid.getChildAt(i);
-//            CalendarView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    if (CardView.getCardBackgroundColor().getDefaultColor() == -1) {
-//                        //Change background color
-//                        CardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
-//                        Toast.makeText(MainActivity.this, "State : True", Toast.LENGTH_SHORT).show();
-//
-//                    } else {
-//                        //Change background color
-//                        CardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
-//                        Toast.makeText(MainActivity.this, "State : False", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
-//        }
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+                        Toast.makeText(MainActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Toast.makeText(MainActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
     private void setSingleEvent(GridLayout mainGrid) {
